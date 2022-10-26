a96de8b93407b945a1aa4f9201c28d9151295603
Руслан Бакиров <baciroff82@gmail.com>
2022-10-26 21:58:59 +0300

    Добавлены: css, img

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 from . import views
 
 
+app_name = 'posts'
+
+
 urlpatterns = [
     # Гдавная страница
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='index'),
+    path('group/', views.group_posts, name='group_list'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
 
-def group_posts(request, slug):
-    return HttpResponse(f'{slug}'
-                        f'Lorem Ipsum - это текст-"рыба", часто используемый '
-                        f'в печати и вэб-дизайне. Lorem Ipsum является '
-                        f'стандартной "рыбой" для текстов на латинице с '
-                        f'начала XVI века. В то время некий безымянный '
-                        f'печатник создал большую коллекцию размеров и форм '
-                        f'шрифтов, используя Lorem Ipsum для распечатки '
-                        f'образцов. Lorem Ipsum не только успешно пережил '
-                        f'без заметных изменений пять веков, но и перешагнул '
-                        f'в электронный дизайн. Его популяризации в новое '
-                        f'время послужили публикация листов Letraset с '
-                        f'образцами Lorem Ipsum в 60-х годах и, в более '
-                        f'недавнее время, программы электронной вёрстки типа '
-                        f'Aldus PageMaker, в шаблонах которых используется '
-                        f'Lorem Ipsum.')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+
+    context ={
+        'title': title,
+        # 'text': slug
+    }
+    return render(request, template, context)
